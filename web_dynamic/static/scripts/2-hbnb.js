9613eb41986b50da359480a534dfe2cc3c43a80b
senseiuc <dhinhojboy@gmail.com>
2023-05-06 09:00:27 +0000

    status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,17 +1,6 @@
 /*
  * js file to select amenities and also get status
  */
-window.addEventListener('load', function () {
-  // task 3
-  $.ajax('http://0.0.0.0:5001/api/v1/status').done(function (data) {
-    if (data.status === 'OK') {
-      $('#api_status').addClass('available');
-    } else {
-      $('#api_status').removeClass('available');
-    }
-  });
-});
-
 $(document).ready(function () {
   $.ajax('http://0.0.0.0:5001/api/v1/status').done(function (data) {
     if (data.status === 'OK') {
