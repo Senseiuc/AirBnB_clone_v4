55a1087768cb3f773172c6b28f298b0e859a2588
SenseiUC <62717218+Senseiuc@users.noreply.github.com>
2023-05-06 09:56:14 +0100

    api get status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,6 +1,16 @@
 /*
  * js file to select amenities and also get status
  */
+window.addEventListener('load', function () {
+  // task 3
+  $.ajax('http://0.0.0.0:5001/api/v1/status').done(function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+});
 
 $(document).ready(function () {
   $.ajax('http://0.0.0.0:5001/api/v1/status').done(function (data) {
